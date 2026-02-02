cae804bbfb523cc90d454f22b8aa571b5979a037
mirkodecillis <mirko.decillis@winelivery.com>
2026-02-02 15:47:13 +0100

    fix: update pre-commit operations

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,7 +4,18 @@ module.exports = {
         'type-enum': [
             2,
             'always',
-            ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'perf']
+            [
+                'feat',     // nuova feature
+                'fix',      // bug fix
+                'docs',     // solo documentazione
+                'style',    // formattazione, no code change
+                'refactor', // refactoring
+                'perf',     // performance
+                'test',     // aggiunta test
+                'chore',    // build, config, etc
+                'revert',   // revert commit
+                'ci',       // CI/CD
+            ],
         ],
         'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case']],
         'header-max-length': [2, 'always', 1000]
